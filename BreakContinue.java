72de34775e31f96d43b2324b0ae08e454f2a8b3b
csherman2828 <csherman2828@gmail.com>
2022-02-09 19:20:49 -0600

    Adjust BreakContinue.java

--- BreakContinue.java
+++ BreakContinue.java
@@ -1,18 +1,17 @@
-
 class BreakContinue {
   public static void main(String[] args) {
     System.out.println("break: ");
     // Use break to exit the loop entirely
     for (int i = 1; i < 6; i++){
-    if (i == 3) {break;}
-    System.out.println(i);
+      if (i == 3) break;
+      System.out.println(i);
     }
 
 
     System.out.println("continue: ");
     // Use continue to "skip" remaining part of one iteration
     for (int i = 1; i < 6; i++){
-      if (i == 3) {continue;}
+      if (i == 3) continue;
       System.out.println(i);
     }
 
