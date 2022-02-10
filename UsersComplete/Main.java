177fbfea9be9a4a471cc957407680092edd4820e
csherman2828 <csherman2828@gmail.com>
2022-02-09 21:08:54 -0600

    Rework the null object check in users example

--- UsersComplete/Main.java
+++ UsersComplete/Main.java
@@ -9,31 +9,31 @@ public class Main {
     };
 
     // // Example of how to use db in this context
+    // // - if there is a user matching the email, get a user object
+    // // - if no user is found, gets null
     // User example = db.find("person.a@example.com");
 
     // start here
     User currentUser;
     for(int i = 0; i < emails.length; i++) {
       currentUser = db.find(emails[i]);
+      if(currentUser != null) {
+        // if(currentUser.isFlagged() == true)
+        if(currentUser.isFlagged()) {
+          System.out.println("Account for " + currentUser + " has been flagged.");
+        }
 
-      // if(currentUser == null)
-      if(currentUser == null) {
-        System.out.println("No user found for " + emails[i]);
-        continue;
-      }
-
-      // if(currentUser.isFlagged() == true)
-      if(currentUser.isFlagged()) {
-        System.out.println("Account for " + currentUser + " has been flagged.");
-      }
-
-      if(currentUser.getBalance() > 0) {
-        System.out.println(
+        if(currentUser.getBalance() > 0) {
+          System.out.println(
           "Account for "
           + currentUser
           + " has an outstanding balance: "
           + currentUser.getBalance()
-        );
+          );
+        }
+      }
+      else {
+        System.out.println("No user account found for " + emails[i]);
       }
     }
   }
